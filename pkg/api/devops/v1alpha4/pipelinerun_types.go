74c47b95220aa9c453110ec6a0485670645ad5b2
johnniang <johnniang@fastmail.com>
2021-08-10 20:49:11 +0800

    Refine PipelineRunSpec

--- pkg/api/devops/v1alpha4/pipelinerun_types.go
+++ pkg/api/devops/v1alpha4/pipelinerun_types.go
@@ -28,8 +28,25 @@ type PipelineRunSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of PipelineRun. Edit pipelinerun_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Organization              string              `json:"organization" description:"name of the organization"`
+	Id                        string              `json:"id" description:"pipeline id - unique within a pipeline"`
+	Pipeline                  string              `json:"pipeline" description:"pipeline name - unique within a pipeline"`
+	Name                      string              `json:"name,omitempty" description:"name of the run"`
+	Description               string              `json:"description,omitempty" description:"description of the run"`
+	ChangeSet                 []ChangeEntry       `json:"changeSet,omitempty" description:"change set of the run"`
+	StartTime                 string              `json:"startTime" description:"start time of the run"`
+	EnQueueTime               string              `json:"enQueueTime" description:"enqueue time of the run"`
+	EndTime                   string              `json:"endTime" description:"end time of the run"`
+	DurationInMillis          int64               `json:"durationInMillis" description:"build duration in milli seconds"`
+	EstimatedDurationInMillis int64               `json:"estimatedDurationInMillis" description:"Estimated build duration in milli seconds"`
+	State                     RunState            `json:"state" description:"the state of the run"`
+	Result                    RunResult           `json:"result" description:"the result state of the job (e.g. unstable)"`
+	RunSummary                string              `json:"runSummary" description:"build summary"`
+	Type                      string              `json:"type" description:"type of the run"`
+	ArtifactsZipFile          string              `json:"artifactsZipFile,omitempty" description:"uri of artifacts zip file"`
+	Causes                    []map[string]string `json:"causes" description:"cause of the run being created"`
+	CauseOfBlockage           string              `json:"causeOfBlockage" description:"cause of what is blocking this run"`
+	Replayable                bool                `json:"replayable" description:"if the run will allow a replay"`
 }
 
 // PipelineRunStatus defines the observed state of PipelineRun
@@ -57,6 +74,51 @@ type PipelineRunList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []PipelineRun `json:"items"`
+}
+
+type ChangeEntry struct {
+	Author        *Author  `json:"author"`
+	CommitId      string   `json:"commit,omitempty" description:"a human readable display name of the commit number, revision number, and such thing that identifies this entry. null if such a concept doesn't make sense for the implementation. For example, in CVS there's no single identifier for commits. Each file gets a different revision number."`
+	Timestamp     string   `json:"timestamp" description:"the timestamp of this commit"`
+	Msg           string   `json:"msg" description:"commit message"`
+	AffectedPaths []string `json:"affectedPaths" description:"a set of paths in the workspace that was affected by this change"`
+	Url           string   `json:"url,omitempty" description:"a browser friendly url to the commit"`
+	Issues        []Issue  `json:"issues" description:"related Issues, like GitHub issues or Jira issues"`
+	CheckoutCount int      `json:"checkoutCount,omitempty" description:"checkout count of current branch"`
+}
+
+type Author struct {
+	Id       string `json:"id" description:"the id of the author"`
+	FullName string `json:"fullName" description:"the name of the author e.g. John Smith"`
+	Email    string `json:"email,omitempty" description:"email address of this author"`
+	Avatar   string `json:"avatar" description:"avatar of this author"`
+}
+
+type RunState string
+
+const (
+	Queued        RunState = "QUEUED"
+	Running       RunState = "RUNNING"
+	Paused        RunState = "PAUSED"
+	Skipped       RunState = "SKIPPED"
+	NotBuiltState RunState = "NOT_BUILT"
+	Finished      RunState = "FINISHED"
+)
+
+type RunResult string
+
+const (
+	Success        RunResult = "SUCCESS"
+	Unstable       RunResult = "UNSTABLE"
+	Failure        RunResult = "FAILURE"
+	NotBuiltResult RunResult = "NOT_BUILT"
+	Unknown        RunResult = "UNKNOWN"
+	Aborted        RunResult = "ABORTED"
+)
+
+type Issue struct {
+	Id  string `json:"id" description:"issue identifier"`
+	Url string `json:"url" description:"issue URL"`
 }
 
 func init() {
